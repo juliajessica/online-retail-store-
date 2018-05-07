5b79df927a70245c932e043b92a49d1824f88633
julia sheremet <juliajessicasher@gmail.com>
2018-05-06 22:56:37 -0700

    create project folder

---
+++ app.routing.ts
@@ -0,0 +1,27 @@
+import { ModuleWithProviders }  from '@angular/core'; //provides our router to the rest of the application
+import { Routes, RouterModule } from '@angular/router'; //contain code that help render specific components when specific URLs are provided to the router
+import { WelcomeComponent }   from './welcome/welcome.component'; //import any components our new route
+import { AboutComponent }   from './about/about.component'; //import about us content to link to the route
+import { MarketplaceComponent }   from './marketplace/marketplace.component';
+import { AlbumDetailComponent }   from './album-detail/album-detail.component';
+
+const appRoutes: Routes = [
+  {
+     path: '',
+     component: WelcomeComponent
+   },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'marketplace',
+    component: MarketplaceComponent
+  },
+  {
+    path: 'albums/:id',
+    component: AlbumDetailComponent
+  }
+ ];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
---
+++ welcome/welcome.component.ts
@@ -0,0 +1,11 @@
+import { Component} from '@angular/core';
+import { Album } from '../model/album.model';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent {
+
+}

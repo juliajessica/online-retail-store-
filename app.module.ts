36d0806521e3ecf0780ab7dcca4f5a4ef3fb20a5
julia sheremet <juliajessicasher@gmail.com>
2018-05-07 21:35:59 -0700

    Add admin footer and html

--- app.module.ts
+++ app.module.ts
@@ -13,6 +13,7 @@ import { routing } from './app.routing';
 import { AboutComponent } from './about/about.component';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
 import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import { AdminComponent } from './admin/admin.component';
 
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
@@ -27,7 +28,8 @@ export const firebaseConfig = {
     WelcomeComponent,
     AboutComponent,
     MarketplaceComponent,
-    AlbumDetailComponent
+    AlbumDetailComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
